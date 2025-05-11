b9606b27883233e84f7305c7e38386e0f5b836b7
Johannes David Hreinsson <46741753+johannes2503@users.noreply.github.com>
2025-05-11 18:04:38 +0000

    Working through logic in exercise

---
+++ car_loan_calculator.md
@@ -0,0 +1,15 @@
+# CAR LOAN CALCULATOR
+
+### Information collected from user
+
+- Loan amount
+  - Validate input
+- Loan duration
+  - Valdate input
+- Loan intere
+
+  - Validate input
+
+- Calculate loan payment per month
+  - Divide interst by months (12)
+  - Divide loan duration by months (12)
--- car_loan_calculator.py
+++ car_loan_calculator.py
@@ -1,7 +1,35 @@
 """
 Car Loan Calculator
 This script calculates the monthly payment for a car \n
-loan based on the loan amount, interest rate, and loan term.
+loan based on the loan amount, interest rate, and loan duration.
+
+You'll need three pieces of information:
+
+    the loan amount
+    the Annual Percentage Rate (APR)
+    the loan duration
+
+From the above, you'll need to calculate the following two things:
+
+    monthly interest rate (APR / 12)
+    loan duration in months
+
+    m = p * (j / (1 - (1 + j) ** (-n)))
+
+    m = monthly payment
+    p = loan amount
+    j = monthly interest rate
+    n = loan duration in months
+
 
 """
 
+print("----------------------------------")
+print('Welcome to JD Car Loan Calculator!')
+print("----------------------------------")
+print("Please enter the loan amount you need in $:")
+loan_amount = input()
+print("Please enter loan duration in years:(The max is 8 years)")
+loan_duration = input()
+print("Please enter interest rate:")
+loan_interest = input()
