a4ac33674de004fac07008a6c20b6ed574a94dfc
Johannes <johannes2503@gmail.com>
2025-09-05 19:23:01 +0000

    finished v2 of rps game

--- Lesson_2_small_programs/rps_game/rock_paper_scissors.py
+++ Lesson_2_small_programs/rps_game/rock_paper_scissors.py
@@ -1,21 +1,45 @@
-"""
-Rock Paper Scissors Game
-"""
 
 import random
 
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = ["rock", "paper", "scissors"]
 
 def prompt(message):
-    print(f'==> {message}')
+    print(f"==> {message}")
 
-prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
-choice = input()
+def display_winner(player, computer):
+    prompt(f"You chose {player}, computer chose {computer}")
 
-while choice not in VALID_CHOICES:
-    prompt("Thats not a valid choice")
+    if ((player == "rock" and computer == "scissors") or
+        (player == "paper" and computer == "rock") or
+        (player == "scissors" and computer == "paper")):
+        prompt("You win!")
+    elif ((player == "rock" and computer == "paper") or
+          (player == "paper" and computer == "scissors") or
+          (player == "scissors" and computer == "rock")):
+        prompt("Computer wins!")
+    else:
+        prompt("It's a tie!")
+
+while True:
+    prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
     choice = input()
 
-computer_choice = random.choice(VALID_CHOICES)
+    while choice not in VALID_CHOICES:
+        prompt("That's not a valid choice")
+        choice = input()
 
-prompt(f"You chose {choice}, computer chose {computer_choice}")
+    computer_choice = random.choice(VALID_CHOICES)
+
+    display_winner(choice, computer_choice)
+
+    while True:
+        prompt("Do you want to play again (y/n)?")
+        answer = input().lower()
+
+        if answer.startswith('n') or answer.startswith('y'):
+            break
+        else:
+            prompt("That's not a valid choice")
+
+    if answer[0] == 'n':
+        break
