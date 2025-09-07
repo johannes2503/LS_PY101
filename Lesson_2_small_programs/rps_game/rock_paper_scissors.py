01d2aa84e5dcf6eb03817dcf758cfa1e1e96cff9
Johannes <johannes2503@gmail.com>
2025-09-07 20:09:46 +0000

    working on bonus no1

--- Lesson_2_small_programs/rps_game/rock_paper_scissors.py
+++ Lesson_2_small_programs/rps_game/rock_paper_scissors.py
@@ -10,14 +10,26 @@ def display_winner(player, computer):
     prompt(f"You chose {player}, computer chose {computer}")
 
     if ((player == "rock" and computer == "scissors") or
+        (player == "rock" and computer == "lizard") or
         (player == "paper" and computer == "rock") or
+        (player == "paper" and computer == "spock") or
         (player == "scissors" and computer == "paper") or
+        (player == "scissors" and computer == "lizard") or
         (player == "lizard" and computer == "paper") or
-        (player == "spock" and computer == "scissors")):
+        (player == "lizard" and computer == "spock") or
+        (player == "spock" and computer == "scissors") or
+        (player == "spock" and computer == "rock")):
         prompt("You win!")
     elif ((player == "rock" and computer == "paper") or
+          (player == "rock" and computer == "spock") or
           (player == "paper" and computer == "scissors") or
-          (player == "scissors" and computer == "rock")):
+          (player == "paper" and computer == "lizard") or
+          (player == "scissors" and computer == "rock") or
+          (player == "scissors" and computer == "spock") or
+          (player == "lizard" and computer == "rock") or
+          (player == "lizard" and computer == "scissors") or
+          (player == "spock" and computer == "paper") or
+          (player == "spock" and computer == "lizard")):
         prompt("Computer wins!")
     else:
         prompt("It's a tie!")
